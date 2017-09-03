eb618ad6f630082013be0f224c4c8cfc42ccd13b
Andrey Naumenko <cympliko@gmail.com>
2017-09-03 19:17:47 +0300

    landing

--- app/src/main/java/com/anontemp/android/Splash.java
+++ app/src/main/java/com/anontemp/android/Splash.java
@@ -1,10 +1,8 @@
 package com.anontemp.android;
 
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
-import android.support.v4.app.ActivityOptionsCompat;
 
 import com.crashlytics.android.Crashlytics;
 
@@ -33,16 +31,7 @@ public class Splash extends FullscreenController {
             public void run() {
                 Intent i = new Intent(Splash.this, WhiteSplash.class);
                 i.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-
-                    ActivityOptionsCompat options = ActivityOptionsCompat.
-                            makeSceneTransitionAnimation(Splash.this, findViewById(R.id.tvLogo), "title");
-                    startActivity(i, options.toBundle());
-                } else {
-                    startActivity(i);
-                }
-
-
+                startActivity(i);
                 finish();
             }
         }, SPLASH_TIME_OUT);
